05126bcb17f232933b10e5988f9dc50c5f21a84f
samanthatoner <samantha.toner@atu.ie>
2023-02-15 22:54:20 -0800

    GGI-38 SG added

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,18 @@ resource "aws_instance" "ec2_instance" {
     instance_type = "${var.instance_type}"
 }
 
+resource "aws_security_group" "ec2_sg" {
+  name        = "GreenGiants_ec2_sg"
+  description = "Security group created for EC2 security group - managed by terraform"
+
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+  }
+}
+
 ######### To do ###########
 #add name
 #add security group with ingress rules 
