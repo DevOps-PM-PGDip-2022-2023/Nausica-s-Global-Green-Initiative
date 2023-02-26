64a09e3924cffca34c6594e2197cf82a0276de6e
Rotimi <roro-commits>
2023-02-26 19:20:38 +0000

    ealstic bean stalk envireoment fix

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -1,10 +1,11 @@
 resource "aws_elastic_beanstalk_application" "application" {
   name = "GreenGiants"
 }
+
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "GreenGiants"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.0.3 running Python 3.7"
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Python 3.8"
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
