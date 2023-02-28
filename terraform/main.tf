699d81229178fd715d2ab5fff5cf7656f2de0ca5
samanthatoner <samantha.toner@atu.ie>
2023-02-28 09:51:00 -0800

    GGI-52 add the monitoring references

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    instance_type = "${var.instance_type}"
+  ami           = "${var.ami_id}"
+  count         = "${var.number_of_instances}"
+  instance_type = "${var.instance_type}"
 }
 
 
@@ -11,9 +11,9 @@ resource "aws_security_group" "ec2_sg" {
 
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
   }
 }
 
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,3 @@
 #Credentials required for creating resources.
-access_key = ""
-secret_key = ""
+access_key = "AKIA3BQR4NR5VENQ6JEU"
+secret_key = "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"
