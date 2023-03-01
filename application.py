ce67d0f944f36e97e9702e84d124a390ab15e2be
Rotimi <roro-commits>
2023-03-01 21:24:59 +0000

    deploying test

--- application.py
+++ application.py
@@ -4,12 +4,11 @@ from auth import search, getrole
 from db_methods import *
 
 
-
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+application = app
 
 
 @app.route("/")
@@ -17,13 +16,15 @@ def main():
     data = getall_grants()
     if not session.get("name"):
         return redirect("/login")
-    return render_template('index.html', name=session.get("name"), role=session.get("role"), data = data)
+    return render_template('index.html', name=session.get("name"), role=session.get("role"), data=data)
+
 
 @app.route("/team")
 def team():
     if not session.get("name"):
         return redirect("/login")
     return render_template('Teamdetails.html', name=session.get("name"), role=session.get("role"))
+
 
 @app.route("/customer")
 def customer():
@@ -32,12 +33,11 @@ def customer():
     return render_template('CustomerDetails.html', name=session.get("name"), role=session.get("role"))
 
 
-
-@app.route("/login", methods=["POST","GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username =  request.form.get("name")
-        password =  request.form.get("password")
+        username = request.form.get("name")
+        password = request.form.get("password")
         if search(username, password):
             session["name"] = username
             session["role"] = getrole(username, password)
@@ -45,7 +45,6 @@ def login():
         else:
             return redirect("/login")
     return render_template('login.html')
-
 
 
 if __name__ == "__main__":
--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -1,14 +1,17 @@
-resource "aws_elastic_beanstalk_application" "application" {
-  name = "GreenGiants"
-}
+### Elastic bean stalk does not make it easy to deploy to already created enviroment 
+##  you have to deplot from the enviroment you created from
 
-resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "GreenGiants"
-  application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Python 3.8"
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = "aws-elasticbeanstalk-ec2-role"
-  }
-}
+# resource "aws_elastic_beanstalk_application" "application" {
+#   name = "GreenGiants"
+# }
+
+# resource "aws_elastic_beanstalk_environment" "environment" {
+#   name                = "GreenGiants"
+#   application         = aws_elastic_beanstalk_application.application.name
+#   solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Python 3.8"
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "IamInstanceProfile"
+#     value     = "aws-elasticbeanstalk-ec2-role"
+#   }
+# }
